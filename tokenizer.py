9cf5e0f539c8a2bc1644403a64d6b62cde7abde8
Andrei-Treil <andrei.treil@gmail.com>
2022-09-20 16:35:15 -0400

    More changes

--- tokenizer.py
+++ tokenizer.py
@@ -10,6 +10,13 @@ So "Ph.D." is not an abbreviation for this project and will result in "Ph" and "
 lines = []
 tokens = []
 
+#helper function to de-acronyze acronyms
+def de_acronym(str):
+    return str.group().replace(".","")
+
 with open('tokenization-input-part-A.txt') as f:
     for line in f:
-        print(re.findall(r'\S+\.\S\.(?=.)',line.replace("'","")))
+        #does not remove _ for some reason?
+        cleaned = re.sub(r'\S+\.\S\.(?=.)',de_acronym,line.replace("'",""))
+        print(re.findall(r'\w+',cleaned))
+        
